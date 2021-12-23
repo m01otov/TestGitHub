f08d885aef9273ad221b83ac5fba8984c707cd72
m01otov <67689912+m01otov@users.noreply.github.com>
2021-12-23 20:47:39 +0500

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Ilya Melnikov on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
